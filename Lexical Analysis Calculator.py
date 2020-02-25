9a23b489749f7a1f037c7e584325e45e63edfd73
Mockingbird1998 <amirrezaei1998@gmail.com>
2020-02-25 23:14:57 +0330

    Second Step

--- Lexical Analysis Calculator.py
+++ Lexical Analysis Calculator.py
@@ -1,11 +1,7 @@
-class Token:
-    pass
+import sys
 
-
-title = ['asg', 'par', 'rpar', 'plus', 'minus', 'mul', 'div']
 buffer = input('Enter phrase : ')
 i = 0
-digit = []
 while i < len(buffer):
     if buffer[i] == ' ':
         i += 1
@@ -17,19 +13,70 @@ while i < len(buffer):
         i += 1
     elif buffer[i].isdigit():
         first = i
-        while buffer[i].isdigit():
+        while i < len(buffer) and buffer[i].isdigit():
             i += 1
+            if buffer[i].isalpha():
+                print('Lexical Error')
+                sys.exit()
+            if buffer[i] == '.':
+                i += 1
+                if not buffer[i].isdigit():
+                    print('Lexical Error')
+                    sys.exit()
+                while i < len(buffer) and buffer[i].isdigit():
+                    i += 1
+                    if buffer[i].isalpha() or buffer[i] == '.':
+                        print('Lexical Error')
+                        sys.exit()
         last = i
-        print('DIGIT : ', buffer[first:last])
+        if '.' in buffer[first:last]:
+            print('DIGIT : ', buffer[first:last])
+        else:
+            print('Decimal: ', buffer[first:last])
     elif buffer[i] == '+':
         print('PLUS : +')
         i += 1
+        if buffer[i] == ' ':
+            i += 1
+        if not (buffer[i].isdigit() or buffer[i] == '(' or buffer[i] == ')'):
+            print('Lexical Error')
+            break
     elif buffer[i] == '-':
         print('MINUS : -')
         i += 1
+        if buffer[i] == ' ':
+            i += 1
+        if not (buffer[i].isdigit() or buffer[i] == '(' or buffer[i] == ')'):
+            print('Lexical Error')
+            break
     elif buffer[i] == '*':
         print('MUL : *')
         i += 1
+        if buffer[i] == ' ':
+            i += 1
+        if not (buffer[i].isdigit() or buffer[i] == '(' or buffer[i] == ')'):
+            print('Lexical Error')
+            break
     elif buffer[i] == '/':
         print('DIV : /')
         i += 1
+        if buffer[i] == ' ':
+            i += 1
+        if not (buffer[i].isdigit() or buffer[i] == '(' or buffer[i] == ')'):
+            print('Lexical Error')
+            break
+    elif buffer[i].isalpha():
+        first = i
+        i += 1
+        while buffer[i].isalpha() or buffer[i].isdigit() or buffer[i] == '_':
+            i = i + 1
+        last = i
+        print('ID : ', buffer[first:last])
+    elif buffer[i] == '=':
+        print('ASG : =')
+        i += 1
+        if buffer[i] == ' ':
+            i += 1
+        if not buffer[i].isalpha() or not buffer[i].isdigit():
+            print('Lexical Error')
+            break
